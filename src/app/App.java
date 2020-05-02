87d518d8ae352af0a62f1fc3d041dfa8aae229a4
Teodor Kostadinov <teodor.kk.93@gmail.com>
2020-05-02 22:12:58 +0300

    Create default window

---
+++ src/app/App.java
@@ -0,0 +1,10 @@
+package app;
+
+import app.ui.MainScreen;
+
+public class App {
+
+    public static void main(String[] args) {
+        new MainScreen();
+    }
+}
---
+++ src/app/ui/MainScreen.java
@@ -0,0 +1,13 @@
+package app.ui;
+
+import javax.swing.*;
+
+public class MainScreen extends JFrame {
+
+    public MainScreen() {
+        setSize(600,600);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
+}
